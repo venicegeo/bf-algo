442d9b637bece1f4ae7bd9961cbd52ab2f9b46d2
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-01 12:01:59 -0400

    Adding deployment scripts.

---
+++ ci/archive.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -ex
+
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
+popd > /dev/null
+
+export GOPATH=$root/gopath
+mkdir -p $GOPATH
+
+source $root/ci/vars.sh
+
+go get -v github.com/venicegeo/$APP/...
+
+src=$GOPATH/bin/bf-service
+
+mv $src $root/$APP.$EXT

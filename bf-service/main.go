26531ca487dbc207847fa447535bed8452cb868c
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-02 00:41:50 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -12,7 +12,7 @@ func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-				cmd1 := exec.Command("/home/vcap/app/build/bin/ossim-info")
+				cmd1 := exec.Command("ls", "-Rlh")
                                 out1, err1 := cmd1.Output()
 
                                 if err1 != nil {

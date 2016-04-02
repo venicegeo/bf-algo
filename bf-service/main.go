ad320500365de6aaaaab68ffe2833fadefa5f43d
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-01 23:18:09 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -12,7 +12,7 @@ func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-				cmd1 := exec.Command("ls", "-alh", "/etc/")
+				cmd1 := exec.Command("cat", "/etc/os-release")
                                 out1, err1 := cmd1.Output()
 
                                 if err1 != nil {

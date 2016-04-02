ce8cc8ba809b84de9dd9b400e218062208b449ec
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-01 21:22:16 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -12,12 +12,11 @@ func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-                                out, err := exec.Command("./ossim-info").Output()
+                                out, err := exec.Command("ls -alh").Output()
                                 if err != nil {
                                         log.Fatal(err)
                                 }
-                                fmt.Fprintf(w, "The date is %s\n", out)
-                                fmt.Fprintf(w, "hello.")
+                                fmt.Fprintf(w, "Cheese %s\n", out)
                         }
                 }
         })

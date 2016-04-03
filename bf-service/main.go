cf323fba60898573dd6323bb7bae040ccba1ba41
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-03 14:28:02 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -12,15 +12,25 @@ func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-				cmd1 := exec.Command("./command.sh")
-                                out1, err1 := cmd1.Output()
+				//cmd1 := exec.Command("./command.sh")
+                                //out1, err1 := cmd1.CombinedOutput()
 
-                                if err1 != nil {
-					log.Fatal(err1.Error())
-                                        fmt.Fprintf(w, "Cheese", err1.Error())
-                                        return
-                                }
-                                fmt.Fprintf(w, string(out1))
+                                //if err1 != nil {
+				//	log.Fatal(err1.Error())
+                                //	fmt.Fprintf(w, "Cheese", err1.Error())
+                                //	return
+                                //}
+                                //fmt.Fprintf(w, string(out1))
+
+				cmd2 := exec.Command("ls", "-alh")
+				out2, err2 := cmd2.CombinedOutput()
+				
+				if err2 != nil {
+					log.Fatal(err2.Error())
+					fmt.Fprintf(w, "Cheese", err2.Error())
+					return
+				}
+				fmt.Fprintf(w, string(out2))
                         }
                 }
         })

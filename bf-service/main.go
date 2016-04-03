5c62b79fe4edd20a9244a237b3bccbbb8cd3efc9
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-03 12:27:04 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -12,7 +12,7 @@ func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-				cmd1 := exec.Command("./command.sh")
+				cmd1 := exec.Command("ls", "-alh")
                                 out1, err1 := cmd1.Output()
 
                                 if err1 != nil {

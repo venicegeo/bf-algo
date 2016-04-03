67b7401f81bad75b747e90291a24af9504c0442f
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-03 14:37:23 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -22,7 +22,7 @@ func main() {
                                 }
                                 fmt.Fprintf(w, string(out1))
 
-				cmd2 := exec.Command("ls", "-alh")
+				cmd2 := exec.Command("./command.sh")
 				out2, err2 := cmd2.CombinedOutput()
 				
 				if err2 != nil {

ddf50a686d08f4c18dd36e2c69f87f3ac77c0060
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-04 08:54:32 -0400

    Testing

--- bf-service/main.go
+++ bf-service/main.go
@@ -4,23 +4,43 @@ import (
         "fmt"
         "log"
         "net/http"
+	"strings"
         "os/exec"
 )
+
+
+func printCommand(cmd *exec.Cmd, w http.ResponseWriter) {
+  fmt.Fprintf(w, "==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+func printError(err error, w http.ResponseWriter) {
+  if err != nil {
+    fmt.Fprintf(w, "==> Error: %s\n", err.Error())
+  }
+}
+
+func printOutput(outs []byte, w http.ResponseWriter) {
+  if len(outs) > 0 {
+    fmt.Fprintf(w, "==> Output: %s\n", string(outs))
+  }
+}
 
 func main() {
 
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 switch r.URL.Path{
                         case "/": {
-				cmd1 := exec.Command("ls", "-alhR")
-                                out1, err1 := cmd1.CombinedOutput()
 
-                                if err1 != nil {
-					log.Fatal(err1.Error())
-                                	fmt.Fprintf(w, "Cheese", err1.Error())
-                                	return
-                                }
-                                fmt.Fprintf(w, string(out1))
+				cmd1 := exec.Command("./ossim-info.bin")
+				printCommand(cmd1, w)
+                                out1, err1 := cmd1.CombinedOutput()
+				printError(err1, w)
+				printOutput(out1, w)
+
+
+
+
+
 
 				//cmd2 := exec.Command("./ossim-info.bin")
 				//out2, err2 := cmd2.CombinedOutput()

92a23c8285ae3aa8c3192d55dd293eab37cea782
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-04 11:25:40 -0400

    Testing

--- bf-service/main.go
+++ bf-service/main.go
@@ -31,7 +31,7 @@ func main() {
                 switch r.URL.Path{
                         case "/": {
 
-				cmd1 := exec.Command("which", "git")
+				cmd1 := exec.Command("ls", "-alhR")
 				printCommand(cmd1, w)
                                 out1, err1 := cmd1.CombinedOutput()
 				printError(err1, w)

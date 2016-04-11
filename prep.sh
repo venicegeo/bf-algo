a2ef0d9855aa8a0ff4e84960d846cf998e98a98c
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-11 12:02:16 -0400

    Testing...

---
+++ prep.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+			
+
+// LC80090472014280LGN00_B3.TIF
+curl -L -o LC80090472014280LGN00_B3.TIF https://www.dropbox.com/s/9e8kk0af5lz5odk/LC80090472014280LGN00_B3.TIF?dl=1
+			
+// LC80090472014280LGN00_B6.TIF
+curl -L -o LC80090472014280LGN00_B6.TIF https://www.dropbox.com/s/nghh5uo711dvhfz/LC80090472014280LGN00_B6.TIF?dl=1
+                                
+
+// LC80150442014002LGN00_B3.TIF
+curl -L -o LC80150442014002LGN00_B3.TIF https://www.dropbox.com/s/867cbaw0vmu7ssn/LC80150442014002LGN00_B3.TIF?dl=1
+                                
+// LC80150442014002LGN00_B6.TIF
+curl -L -o LC80150442014002LGN00_B6.TIF https://www.dropbox.com/s/a7scvz5jdaytc39/LC80150442014002LGN00_B6.TIF?dl=1
+                             
+
+// LC80340432016061LGN00_B3.TIF
+curl -L -o LC80340432016061LGN00_B3.TIF https://www.dropbox.com/s/ejoydxx6zembuxe/LC80340432016061LGN00_B3.TIF?dl=1
+                            
+// LC80340432016061LGN00_B6.TIF
+curl -L -o LC80340432016061LGN00_B6.TIF https://www.dropbox.com/s/laewldtba1j73f4/LC80340432016061LGN00_B6.TIF?dl=1
+                              
+	
+// LC81190532015078LGN00_B3.TIF
+curl -L -o LC81190532015078LGN00_B3.TIF https://www.dropbox.com/s/6vcjay8zrp2l0zo/LC81190532015078LGN00_B3.TIF?dl=1
+                               
+// LC81190532015078LGN00_B6.TIF
+curl -L -o LC81190532015078LGN00_B6.TIF https://www.dropbox.com/s/tx8ganamkiw3zm3/LC81190532015078LGN00_B6.TIF?dl=1
+                             
+
+// LC81600422014314LGN00_B3.TIF
+curl -L -o LC81600422014314LGN00_B3.TIF https://www.dropbox.com/s/h2rlbovzfo5h1ki/LC81600422014314LGN00_B3.TIF?dl=1
+
+// LC81600422014314LGN00_B6.TIF
+curl -L -o LC81600422014314LGN00_B6.TIF https://www.dropbox.com/s/a90cmi2ommp1z9w/LC81600422014314LGN00_B6.TIF?dl=1
+                               
+
+// LC82010352014217LGN00_B3.TIF
+curl -L -o LC82010352014217LGN00_B3.TIF https://www.dropbox.com/s/bos9qa9w6fvo7sq/LC82010352014217LGN00_B3.TIF?dl=1
+                               
+// LC82010352014217LGN00_B6.TIF
+curl -L -o LC82010352014217LGN00_B6.TIF https://www.dropbox.com/s/31um105g38t1c5u/LC82010352014217LGN00_B6.TIF?dl=1
+                            

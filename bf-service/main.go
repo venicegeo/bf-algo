0c3438bc34b1cc478b9900c9ad8fece372f56b51
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-11 18:02:41 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -28,20 +28,20 @@ func printOutput(outs []byte, w http.ResponseWriter) {
 func main() {
 
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                switch r.URL.Path{
-			case "/ls": {
+                switch r.URL.Query(){
+			case "ls": {
 				cmdLs := exec.Command("ls", "-alh")
 				outLs, errLs := cmdLs.Output()
 				printError(errLs, w)
 				printOutput(outLs, w)
 			}
-			case "/prep": {
+			case "prep": {
 				cmd0 := exec.Command("./prep.sh")
                                 out0, err0 := cmd0.CombinedOutput()
                                 printError(err0, w)
                                 printOutput(out0, w)
 			}
-			case "/case1": {
+			case "case1": {
 				cmd0 := exec.Command("curl", "-L", "-o", "LC80090472014280LGN00_B3.TIF", "https://www.dropbox.com/s/9e8kk0af5lz5odk/LC80090472014280LGN00_B3.TIF?dl=1")
 				cmd0.Run()
 				cmd1 := exec.Command("curl", "-L", "-o", "LC80090472014280LGN00_B6.TIF", "https://www.dropbox.com/s/nghh5uo711dvhfz/LC80090472014280LGN00_B6.TIF?dl=1")
@@ -58,7 +58,7 @@ func main() {
 				cmd4 := exec.Command("rm", "LC80090472014280LGN00_B3.TIF", "LC80090472014280LGN00_B6.TIF")
 				cmd4.Run()
 			}
-			case "/case2": {
+			case "case2": {
 
                                 cmd1 := exec.Command("curl", "-L", "-o", "LC80150442014002LGN00_B6.TIF", "https://www.dropbox.com/s/a7scvz5jdaytc39/LC80150442014002LGN00_B6.TIF?dl=1")
                                 cmd1.Run()
@@ -73,13 +73,8 @@ func main() {
 
 				cmd4 := exec.Command("rm", "LC80150442014002LGN00_B3.TIF", "LC80150442014002LGN00_B6.TIF")
                                 cmd4.Run()
-
-				cmd5 := exec.Command("df", "-h")
-                                out5, err5 := cmd5.CombinedOutput()
-                                printError(err5, w)
-                                printOutput(out5, w)
 			}
-			case "/case3": {
+			case "case3": {
 				cmd0 := exec.Command("curl", "-L", "-o", "LC80340432016061LGN00_B3.TIF", "https://www.dropbox.com/s/ejoydxx6zembuxe/LC80340432016061LGN00_B3.TIF?dl=1")
                                 cmd0.Run()
                                 cmd1 := exec.Command("curl", "-L", "-o", "LC80340432016061LGN00_B6.TIF", "https://www.dropbox.com/s/laewldtba1j73f4/LC80340432016061LGN00_B6.TIF?dl=1")
@@ -96,7 +91,7 @@ func main() {
 				cmd4 := exec.Command("rm", "LC80340432016061LGN00_B3.TIF", "LC80340432016061LGN00_B6.TIF")
                                 cmd4.Run()
 			}
-			case "/case4": {
+			case "case4": {
 				cmd0 := exec.Command("curl", "-L", "-o", "LC81190532015078LGN00_B3.TIF", "https://www.dropbox.com/s/6vcjay8zrp2l0zo/LC81190532015078LGN00_B3.TIF?dl=1")
                                 cmd0.Run()
                                 cmd1 := exec.Command("curl", "-L", "-o", "LC81190532015078LGN00_B6.TIF", "https://www.dropbox.com/s/tx8ganamkiw3zm3/LC81190532015078LGN00_B6.TIF?dl=1")
@@ -113,7 +108,7 @@ func main() {
 				cmd4 := exec.Command("rm", "LC81190532015078LGN00_B3.TIF", "LC81190532015078LGN00_B6.TIF")
                                 cmd4.Run()
 			}
-			case "/case5": {
+			case "case5": {
 				cmd0 := exec.Command("curl", "-L", "-o", "LC81600422014314LGN00_B3.TIF", "https://www.dropbox.com/s/h2rlbovzfo5h1ki/LC81600422014314LGN00_B3.TIF?dl=1")
                                 cmd0.Run()
                                 cmd1 := exec.Command("curl", "-L", "-o", "LC81600422014314LGN00_B6.TIF", "https://www.dropbox.com/s/a90cmi2ommp1z9w/LC81600422014314LGN00_B6.TIF?dl=1")
@@ -130,7 +125,7 @@ func main() {
 				cmd4 := exec.Command("rm", "LC81600422014314LGN00_B3.TIF", "LC81600422014314LGN00_B6.TIF")
                                 cmd4.Run()
 			}
-			case "/case6": {
+			case "case6": {
 				cmd0 := exec.Command("curl", "-L", "-o", "LC82010352014217LGN00_B3.TIF", "https://www.dropbox.com/s/bos9qa9w6fvo7sq/LC82010352014217LGN00_B3.TIF?dl=1")
                                 cmd0.Run()
                                 cmd1 := exec.Command("curl", "-L", "-o", "LC82010352014217LGN00_B6.TIF", "https://www.dropbox.com/s/31um105g38t1c5u/LC82010352014217LGN00_B6.TIF?dl=1")

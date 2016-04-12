695078f1b8eb7f2710cf3adf849abf14a2677386
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-12 11:25:13 -0400

    Testing...

--- bf-service/main.go
+++ bf-service/main.go
@@ -29,7 +29,7 @@ func printOutput(outs []byte, w http.ResponseWriter) {
 func main() {
 
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                switch r.URL.Query().Get("cmd"){
+                switch r.URL.Query().Get("cheese"){
 			case "ls": {
 				cmdLs := exec.Command("ls", "-alh")
 				outLs, errLs := cmdLs.Output()
